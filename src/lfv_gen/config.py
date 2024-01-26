15ccf421a5638ee9af96122e68c828d5b2b7af8c
Daniel CH Tan <dtch1997@users.noreply.github.com>
2024-01-26 14:34:38 +0000

    Fix pyright errors, ignore experiments for pyright

--- src/lfv_gen/config.py
+++ src/lfv_gen/config.py
@@ -3,4 +3,4 @@ import os
 
 ROOT_DIR = pathlib.Path(__file__).parent.parent.parent
 DATASETS_DIR = pathlib.Path(os.getenv("DATASETS_DIR", "datasets"))
-JAM_MODEL_DIR = pathlib.Path(os.getenv("JAM_MODEL_DIR"), "data/models")
+JAM_MODEL_DIR = pathlib.Path(os.getenv("JAM_MODEL_DIR", "data/models"))
